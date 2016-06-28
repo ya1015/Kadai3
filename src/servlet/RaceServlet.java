556c0c55c1827f4ac1487214ae53a7a5ef95f4ce
yamaguchi <firstippo1015@gmail.com>
2016-06-28 12:58:22 +0900

    Second Commit

--- src/servlet/RaceServlet.java
+++ src/servlet/RaceServlet.java
@@ -20,14 +20,16 @@ public class RaceServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/** 電気自動車の走行を表す文字列 */
-	private static String etext = "";
+	private String etext = "";
 	/** バスの走行を表す文字列 */
-	private static String btext = "";
+	private String btext = "";
 	/** スーパーカーの走行を表す文字列 */
-	private static String stext = "";
+	private String stext = "";
 	/** F5を押された回数を数えるカウンター */
-	private static int counter;
-	
+	private int counter;
+	/** 休憩を表すフラグ */
+	private boolean eRest_flag = false;
+
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
@@ -36,28 +38,57 @@ public class RaceServlet extends HttpServlet {
 	}
 
 	/**
-S	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * S * @see HttpServlet#doGet(HttpServletRequest request,
+	 * HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// 文字コード設定
 		request.setCharacterEncoding("utf-8");
-		
-			ElectricCar ec = new ElectricCar();
-			etext += ec.getSpeed();
+
+		if (request.getParameter("flg") == null) {
+			System.out.println("fjdfhfdhf;a");
+		} else if(request.getParameter("flg") == null){
+			// counterが5になったときに1回休み、2分の1の確率で復活
+			if (counter % 5 != 0) {
+				ElectricCar ec = new ElectricCar();
+				etext += ec.getSpeed();
+			} else {
+				eRest_flag = true;
+			}
+			if (counter % 5 != 0 && eRest_flag) {
+				if (Math.random() <= 0.5)
+					eRest_flag = !eRest_flag;
+			}
 			request.setAttribute("etext", etext + "E");
 
-			Bus b = new Bus();
-			btext += b.getSpeed();
+			// couterが3になったときに1回休み、10分の3の確率で2.5倍の速さ
+			if (counter % 3 != 0) {
+				Bus b = new Bus();
+				if (Math.random() <= (10 / 3)) {
+					btext += "＊＊＊＊＊";
+				} else {
+					btext += b.getSpeed();
+				}
+			}
 			request.setAttribute("btext", btext + "B");
 
-			SuperCar sc = new SuperCar();
-			stext += sc.getSpeed();
+			// 2分の1の確率で休み、5分の1の確率で1.5倍の速さ
+			if (Math.random() <= 0.5) {
+				if (Math.random() <= 0.2) {
+					stext += "＝＝＝＝＝＝";
+				} else {
+					SuperCar sc = new SuperCar();
+					stext += sc.getSpeed();
+				}
+			}
 			request.setAttribute("stext", stext + "S");
-		
-		
-		request.getRequestDispatcher("output.jsp").forward(request, response);
+
+			// カウントアップ
+			counter++;
+
+			request.getRequestDispatcher("output.jsp").forward(request, response);
+		}
 
 	}
 
